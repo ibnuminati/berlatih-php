2591eee8c793bc312791e6eef62f6c1ab1825968
Ibnu Fajar Alam <68974861+ibnuminati@users.noreply.github.com>
2020-07-30 10:30:03 +0700

    Update palindrome-angka.php

--- palindrome-angka.php
+++ palindrome-angka.php
@@ -1,7 +1,24 @@
 <?php
 
+function palindrome($str){
+	$output = false;
+	if (strrev($str) == $str){
+		$output = true;
+	}
+	return $output;
+}
+
 function palindrome_angka($angka) {
-  // tulis kode di sini
+  if($angka >= 1 && $angka <= 8){
+  	return $angka + 1;
+  	}
+  if(palindrome($angka)){
+  	$angka++;
+  	}
+  while(palindrome($angka) == false){
+  	$angka++;
+  	}
+  	return $angka;
 }
 
 // TEST CASES
